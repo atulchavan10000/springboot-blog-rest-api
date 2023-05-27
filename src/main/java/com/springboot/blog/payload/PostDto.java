930217911fd65e55181a0eab5cf97ed9019774de
Atul <atulchavan528@gmail.com>
2023-05-27 18:12:18 +0530

    validation of create/update Post api's with spring-boot-starter-validation

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -1,4 +1,7 @@
 package com.springboot.blog.payload;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Set;
@@ -6,8 +9,16 @@ import java.util.Set;
 @Data
 public class PostDto {
     private Long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters")
     private String title;
+
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters")
     private String description;
+
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 }
