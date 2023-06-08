b9953043a8a55b24a0482f3a0a163c181bb68da7
Atul <atulchavan528@gmail.com>
2023-06-08 18:48:08 +0530

    implementing create and get category api

---
+++ src/main/java/com/springboot/blog/controller/CategoryController.java
@@ -0,0 +1,34 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.CategoryDto;
+import com.springboot.blog.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    // Build add category REST api
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CategoryDto> addCategory(@RequestBody CategoryDto categoryDto){
+        CategoryDto savedCategoryDto = categoryService.addCategory(categoryDto);
+        return new ResponseEntity<>(savedCategoryDto, HttpStatus.CREATED);
+    }
+
+    // get category REST api
+    @GetMapping("{id}")
+    public ResponseEntity<CategoryDto> getCategory(@PathVariable(name ="id") Long categoryId){
+        CategoryDto categoryDto = categoryService.getCategory(categoryId);
+        return ResponseEntity.ok(categoryDto);
+    }
+}
